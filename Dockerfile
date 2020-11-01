81a39b79f1490e35ce6f5381bd8793efe32ae6fa
edwinb-ai <developeredwin@gmail.com>
2020-10-31 18:42:25 -0600

    :sparkles:
    Se crea el contenedor desde cero. Además se agrega un archivo de
    configuración especial.

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM docker://gitlab/gitlab-ce:latest
+
+RUN mkdir -p /var/www/letsencrypt
+RUN mkdir -p /var/www/pagessl
+
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install dirmngr --install-recommends -y
+RUN apt-get install software-properties-common -y
+
+RUN apt-get install snapd -y
+RUN snap install core && snap refresh core
+
+RUN snap install --classic certbot
+RUN ln -s /snap/bin/certbot /usr/bin/certbot
+RUN certbot --nginx
+
+RUN gitlab-ctl reconfigure
+RUN certbot certonly --webroot --webroot-path=/var/www/letsencrypt -d milabproy.redclara.net
+
+COPY gitlab.rb /etc/gitlab
--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 # a los permisos en el servidor
 GITLAB_HOME=$HOME/gitlab-test/srv/gitlab
 
+mkdir -p ${GITLAB_HOME}
+
 # * IP y hostname
-HOST_NAME="ilabproy.redclara.net"
+HOST_NAME="milabproy.redclara.net"
 IP_HOST=201.159.223.183
 
 # * Puertos
@@ -12,25 +14,37 @@ HTTP_PORT=80
 HTTPS_PORT=443
 
 # * Volumenes
+HOST_CONFIG=$HOME/gitlab-test/etc/gitlab
 CONFIG=$GITLAB_HOME/config
+HOST_LOGS=$HOME/gitlab-test/var/log/gitlab
 LOGS=$GITLAB_HOME/logs
+HOST_DATA=$HOME/gitlab-test/var/opt/gitlab
 DATA=$GITLAB_HOME/data
+
+# * Crear los directorios no existentes
+mkdir -p ${CONFIG}
+mkdir -p ${HOST_CONFIG}
+mkdir -p ${HOST_LOGS}
+mkdir -p ${LOGS}
+mkdir -p ${HOST_DATA}
+mkdir -p ${DATA}
 
 # * Imagen de Docker
 GITLAB_DOCKER="docker.io/gitlab/gitlab-ce"
+# GITLAB_DOCKER="gitlab:v1.2"
 
 # * Comando para ejecutar el contenedor
 # Primero, exponer la variable GITLAB_HOME
 export GITLAB_HOME
 
 # Luego, ejecutar el contenedor
-docker run --detach \
+sudo podman run --detach \
 --hostname $HOST_NAME \
 --publish $IP_HOST:$HTTP_PORT:$HTTP_PORT \
 --publish $IP_HOST:$HTTPS_PORT:$HTTPS_PORT \
---publish $IP_HOST:8065:22 \ # Este puerto es especial para pruebas
+--publish $IP_HOST:8065:22 \
 --name gitlab \
---volume $CONFIG:$HOME/gitlab-test/etc/gitlab \
---volume $LOGS:$HOME/gitlab-test/var/log/gitlab \
---volume $DATA:$HOME/gitlab-test/var/opt/gitlab \
+--volume $CONFIG:$HOST_CONFIG \
+--volume $LOGS:$HOST_LOGS \
+--volume $DATA:$HOST_DATA \
 $GITLAB_DOCKER
